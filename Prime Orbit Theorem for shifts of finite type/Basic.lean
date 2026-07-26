30d68c6310539ff8f0cb2334e3aa221114dea527
Akira Corr <sasakiaqc@example.com>
2026-07-26 13:38:42 +0100

    basic defs

--- Prime Orbit Theorem for shifts of finite type/Basic.lean
+++ Prime Orbit Theorem for shifts of finite type/Basic.lean
@@ -1,1 +1,13 @@
-def hello := "world"
+import Mathlib.Dynamics.SymbolicDynamics.Basic
+import Mathlib.Dynamics.PeriodicPts.Defs
+import Mathlib.Dynamics.PeriodicPts.Lemmas
+import Mathlib.LinearAlgebra.Matrix.Trace
+
+variable {k : ℕ}
+
+abbrev TransitionMatrix (k : ℕ) := Matrix (Fin k) (Fin k) Bool
+abbrev FullShift (k : ℕ) := ℤ -> Fin k
+
+def Shift (x : FullShift k) : FullShift k := fun n => x (n + 1)
+def ShiftOfFiniteType (A : TransitionMatrix k) : Set (FullShift k) :=
+  {x | ∀ n : ℤ, A (x n) (x (n + 1)) = true}
