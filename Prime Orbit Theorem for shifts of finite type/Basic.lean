f38c6aae76905e45a5f9966839d0284bfc26d10c
Akira Corr <sasakiaqc@example.com>
2026-07-27 15:27:33 +0100

    def primeOrbits

--- Prime Orbit Theorem for shifts of finite type/Basic.lean
+++ Prime Orbit Theorem for shifts of finite type/Basic.lean
@@ -15,22 +15,39 @@ abbrev TransitionMatrix (k : ℕ) := Matrix (Fin k) (Fin k) Bool
 abbrev FullShift (k : ℕ) := ℤ -> Fin k
 
 def shift (x : FullShift k) : FullShift k := fun n => x (n + 1)
+notation "σ" => shift
+
 def ShiftOfFiniteType (A : TransitionMatrix k) : Set (FullShift k) :=
   {x | ∀ n : ℤ, A (x n) (x (n + 1)) = true}
+notation "SoFT" => ShiftOfFiniteType
 
 /- Shift invariance of a SoFT -/
 theorem shift_mapsTo (A : TransitionMatrix k) :
-    Set.MapsTo shift (ShiftOfFiniteType A) (ShiftOfFiniteType A) := by
+    Set.MapsTo σ (SoFT A) (SoFT A) := by
   exact Set.mapsTo_iff_subset_preimage.mpr fun ⦃a⦄ a_1 n ↦ a_1 (n + 1)
 
 variable (A : TransitionMatrix k)
 
 open Function
 
-/- Fix_n -/
-def Fix (n : ℕ) : Set (FullShift k) := ShiftOfFiniteType A ∩ ptsOfPeriod shift n
-#check periodicOrbit shift /- τ -/
+/- Useful defns in Dynamics.PeriodicPts -/
+#check periodicOrbit /- All prime orbits in the FullShift -/
 #check isPeriodicPt_iff_minimalPeriod_dvd /- minimalperiod | period -/
+#check periodicPts /- Set of all periodic points -/
+#check ptsOfPeriod /- Set of periodic pts given a period (minimal or not) -/
+
+/- Fix_n -/
+def Fix (n : ℕ) : Set (FullShift k) := SoFT A ∩ ptsOfPeriod σ n
 
 /- x lies in prime orbit of period n -/
-def IsPrimeOrbitOf (n : ℕ) (x : FullShift k) : Prop := minimalPeriod shift x = n
+def IsPrimeOrbitOf (n : ℕ) (x : FullShift k) : Prop := minimalPeriod σ x = n
+
+/- Set of all prime orbits in SoFT. To be used for indexing sums later -/
+noncomputable def primeOrbits : Set (Cycle (FullShift k)) :=
+  (fun x => periodicOrbit σ x) '' (SoFT A  ∩ periodicPts σ)
+
+def orbitPeriod (x : FullShift k) : ℕ := sorry
+notation "λ" => orbitPeriod
+
+def primeOrbitCount (n : ℕ) : ℕ := sorry
+notation "π" => primeOrbitCount
